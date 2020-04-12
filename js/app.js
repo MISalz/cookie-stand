29af7b19dbb585e840eb9e2a4af80490017fb975
Michelle Salazar <michellesalazar010@gmail.com>
2020-04-12 02:01:41 -0700

    update js file

--- js/app.js
+++ js/app.js
@@ -8,8 +8,15 @@
 // Display the values of each array as unordered lists in the browser
 // Calculating the sum of these hourly totals
 
+var pSalEl = document.getElementById('sales-table');
+var HoursOP = ['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm'];
+//Generate random number function
+for(var i = 0; i < HoursOP.length; i++){
+  var trb = document.createElement('tr');
+  trb.textContent = HoursOP[i];
+  pSalEl.appendChild(trb);
+}
 
-var HoursOP =['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3pm','4pm','5pm','6pm','7pm',];
 
 var Store1 = {
   Location: 'Seattle',
@@ -17,22 +24,29 @@ var Store1 = {
   MaxCust: 65,
   AvgCookieSale: 6.3,
   HoursofOperation: '6:00am to 8:00pm',
-  //Generate random number function
   randomizerCH: function() {
-    var RandomizerCH = Math.floor(Math.random()*(this.MaxCust-this.MinCust)+1+this.MinCust);
+    for (var i=0; i < HoursOP.length; i++)
+      var RandomizerCH = Math.floor(Math.random()*(this.MaxCust-this.MinCust)+1+this.MinCust);
+    console.log(RandomizerCH);
     return RandomizerCH;
   },
-  //calculate simulated cookies purchased for each hour
   custPerHourcal: function() {
-    for(var i=0; i<HoursOP.length; i++){
+    for(var i=0; i <HoursOP.length; i++){
       var custPerHourcal = this.AvgCookieSale * this.randomizerCH();
+      console.log(custPerHourcal);
       return Math.round(custPerHourcal);
     }
   },
 };
 Store1.custPerHourcal();
 Store1.randomizerCH();
+for(var j = 0; j < HoursOP.length; j++){
+  var trb2 = document.createElement('td');
+  trb2.textContent = Store1.randomizerCH();
+  pSalEl.appendChild(trb2);
+}
 
+/*
 var Store2 = {
   Location: 'Tokyo',
   MinCust: 3,
@@ -125,3 +139,4 @@ var Store5 = {
 };
 Store5.custPerHourcal();
 Store5.randomizerCH();
+*/
